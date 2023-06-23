b8303ceb273be14ec4c95f161d17208295bc2b43
rene-grigori <rene.tstsocal@gmail.com>
2023-06-23 08:56:48 -0700

    refactor: adding my touches

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-import "./main.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
 
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
-import App from "./App";
-
-const container = document.getElementById("root");
-const root = createRoot(container as HTMLDivElement);
+const container = document.getElementById('root')
+const root = createRoot(container as HTMLDivElement)
 
 root.render(
   <StrictMode>
     <App />
   </StrictMode>
-);
+)
